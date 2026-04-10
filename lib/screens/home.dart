c9a00060a8833574495a25d3b6a4f78f4adf53c0
mattiabovo <mattia.bovo.1@studenti.unipd.it>
2026-04-10 10:04:32 +0200

    Refactor HomePage layout and update route name

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'login.dart';
+
+import './LoginPage.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
-  static const routename = 'Homepage';
+  static const routename = 'HomePage';
 
   @override
   Widget build(BuildContext context) {
@@ -14,14 +15,21 @@ class HomePage extends StatelessWidget {
         title: Text(HomePage.routename),
       ),
       body: Center(
-        child:        
-      ElevatedButton(
-         onPressed : (){
-              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginPage()));
-          },
-          child: Text('To the login page'),
-    ),
-    ),);
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: Text('To the LoginPage'),
+              onPressed: () {
+                Navigator.pushReplacement(context, MaterialPageRoute(builder: ((context) => LoginPage())));
+              },
+            ),
+            
+            
+          ],
+        ),
+      ),
+    );
   } //build
 
-}
+} //LoginPage
