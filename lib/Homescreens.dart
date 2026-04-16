e774b6e23cbd472de9b2bf59bb97eccd4891340d
silviarigo1 <silvia.rigo.1@studenti.unipd.it>
2026-04-16 09:38:13 +0200

    Update Homescreens.dart

--- lib/Homescreens.dart
+++ lib/Homescreens.dart
@@ -1,1 +1,56 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int selectedIndex = 0;
+
+  // List of pages
+  final List<Widget> pages = [
+    Center(child: Text("Home Page")),
+    Center(child: Text("Travel Page")),
+    Center(child: Text("Profile Page")),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[selectedIndex],
+
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: selectedIndex,
+
+        // When user taps an item
+        onDestinationSelected: (int index) {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+
+        destinations: [
+          NavigationDestination(
+            icon: Icon(Icons.home_outlined),
+            selectedIcon: Icon(Icons.home),
+            label: "Home",
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.airplanemode_active),
+            selectedIcon: Icon(Icons.airplanemode_active),
+            label: "Travel",
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.person_2_rounded),
+            selectedIcon: Icon(Icons.person_2_rounded),
+            label: "Profile",
+          ),
+        ],
+      ),
+    );
+  }
+}
 
