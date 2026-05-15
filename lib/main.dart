a2d2c17135128d8be4d1a37413e88e157d722fa8
mattiabovo <mattia.bovo.1@studenti.unipd.it>
2026-05-15 11:22:19 +0200

    Add comment to MultiProvider in main.dart
    Added comment for clarification on MultiProvider usage.

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
+    return MultiProvider( //vedi esempio prof lezione 15 maggio
       providers: [
         ChangeNotifierProvider(create: (context) => ResultSwipe(), ),
        // ChangeNotifierProvider(create: (context) => AimsProvider()), 
