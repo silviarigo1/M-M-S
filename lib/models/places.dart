1b755876bae2b2ab5a787bb6eff1592a9a4c2d33
Silvia <silvia.rigo.1@studenti.unipd.it>
2026-06-21 16:36:55 +0200

    mete di ricarica

--- lib/models/places.dart
+++ lib/models/places.dart
@@ -7,16 +7,14 @@ class Places{
     
   ];
   static const List<String> images = [
-    // "lib/images/scudetti/PadovaCalcio.png",
     "lib/images/simbolo_padova.png",
-
   ];
 
- static const List<int> _steps = [200, 3000, 1000, 2000, 5000, 2000, 1500, 0, 500, 2000];
-  static const List<double> _hours = [0.25, 1.5, 1, 1, 3, 1, 0.5, 0.5, 1, 1.5];
-  static const List<int> _crowded = [1, 3, 5, 4, 5, 3, 4, 6, 2, 1];
-  static const List<bool> _opened = [false, false, true, false, true, false, true, false, false, false];
-  static const List<bool> _charge = [false, false, false, false,false, false,false, true,false, false];
+ static const List<int> _steps = [200, 200, 3000, 0, 1000, 2000, 5000, 2000, 1500, 0, 500, 2000, 0 ];
+  static const List<double> _hours = [0.25, 1.0, 1.5, 0.5, 1, 1, 3, 1, 0.5, 0.5, 1, 1.5, 1.5];
+  static const List<int> _crowded = [1, 2, 3, 2, 5, 4, 5, 3, 4, 6, 2, 1, 3];
+  static const List<bool> _opened = [false, true, false, false,  true, false, true, false, true, false, false, false, false];
+  static const List<bool> _charge = [false, true, false, true, false, false,false, false,false, true,false, false, true];
 
   // 2. Creiamo la lista batt calcolandola direttamente dai dati sopra
   static List<int> batt = List.generate(_steps.length, (i) {
@@ -26,7 +24,9 @@ class Places{
 
 
  static Map<String, List<dynamic>> mapDest = {"title": ["Cappella degli Scrovegni", 
+    "Giardini dell'Arena",
     "Basilica di sant'Antonio", 
+    "Pasticceria Breda",
     "Prato della Valle", 
     "Palazzo della ragione", 
     "Orto Botanico", 
@@ -34,9 +34,13 @@ class Places{
     "Piazza delle Erbe e della Frutta",
     "Caffè Pedrocchi",
     "Duomo e Battistero di San Giovanni",
-    "La Specola"],
+    "La Specola",
+    "Osteria Nane della Giulia"
+  ],
     "description": ["Questo gioiello dell'arte trecentesca custodisce il celebre ciclo di affreschi di Giotto, considerato una pietra miliare della pittura occidentale. Le pareti raccontano la storia di Maria e di Cristo, culminando nel maestoso Giudizio Universale. È un'esperienza immersiva unica, capace di lasciare ogni visitatore senza fiato per la vivacità dei colori e la modernità espressiva.",
+    "I Giardini dell'Arena di Padova fondono un ricco passato storico, racchiusi tra i resti dell'anfiteatro romano, le storiche mura trecentesche e la celebre Cappella degli Scrovegni di Giotto. Inoltre, offre la possbilità di fare una piacevole sosta con un aperitivo o un caffè in uno dei chioschi del parco.",
     "Conosciuta dai padovani semplicemente come 'Il Santo', questa imponente basilica fonde stili romanico, gotico e bizantino in un'architettura armoniosa. Al suo interno sono custodite le reliquie del Santo e splendide opere d'arte di Donatello, tra cui l'altare maggiore. Le sue otto cupole e i campanili svettanti la rendono uno dei simboli più riconoscibili e visitati della città.",
+    "Pasticceria storica di Padova, fondata nel 1967. Famosa per i suoi dolci tradizionali, tra cui la Torta Pazientina. Il luogo ideale per una sosta veloce e piacevole, non lontana dal centro città, perfetta per ricaricare le energie dopo una giornata di visite.",
     "Con i suoi quasi 90.000 metri quadrati, è una delle piazze più grandi d'Europa e un vero capolavoro urbanistico del XVIII secolo. Al centro si trova l'Isola Memmia, circondata da un canale ellittico ornato da 78 statue che ritraggono personaggi illustri legati alla città. È il cuore pulsante della vita cittadina, ideale per una passeggiata rilassante tra storia, mercati e architettura monumentale.",
     "Antica sede dei tribunali cittadini, questo palazzo medievale separa Piazza delle Erbe da Piazza della Frutta ed è famoso per il suo enorme salone pensile. Le pareti della sala superiore sono interamente decorate da un ciclo di affreschi a tema astrologico e religioso tra i più vasti al mondo. Al piano terra, sotto i voltoni, si trova ancora oggi lo storico mercato coperto, ricco di botteghe enogastronomiche tradizionali.",
     "Fondato nel 1545, è l'orto botanico universitario più antico del mondo ancora situato nella sua collocazione originaria e fa parte del Patrimonio UNESCO. Al suo interno si possono ammirare piante rare, esemplari storici come la 'Palma di Goethe' e il modernissimo Giardino della Biodiversità. È un luogo magico dove la ricerca scientifica secolare si sposa perfettamente con la bellezza e la varietà della natura.",
@@ -44,14 +48,17 @@ class Places{
     "Queste due piazze gemelle, divise dal maestoso Palazzo della Ragione, rappresentano da secoli il cuore commerciale e sociale della città. Di giorno ospitano vivaci mercati all'aperto, mentre all'imbrunire si trasformano nel tempio del rito dell'aperitivo padovano. L'atmosfera che si respira tra i banchi di frutta e i tavolini dei bar è l'essenza più autentica e vibrante della vita cittadina.",
     "Conosciuto come il 'caffè senza porte' perché un tempo aperto giorno e notte, è uno dei caffè storici più famosi d'Italia e simbolo del Risorgimento. L'architettura eclettica unisce stili neoclassici e gotici, offrendo sale eleganti dove un tempo si riunivano intellettuali e studenti. È d'obbligo assaggiare il celebre 'Caffè Pedrocchi', servito in tazza grande con crema di menta e una spolverata di cacao, rigorosamente senza zucchero.",
     "Sebbene la Cattedrale sia imponente, il vero tesoro si trova nel Battistero adiacente, interamente decorato da un ciclo di affreschi trecenteschi di Giusto de' Menabuoi. Le scene bibliche coprono ogni centimetro delle pareti e della cupola, con un Paradiso circolare che lascia senza parole per precisione e colori. È considerato uno dei capolavori meglio conservati dell'arte pittorica medievale, spesso ingiustamente messo in ombra dalla Cappella degli Scrovegni.",
-    "Situata in un'antica torre del castello dei Carraresi, questa struttura è stata trasformata nel XVIII secolo in un osservatorio astronomico all'avanguardia. Oggi ospita un affascinante museo dove è possibile ammirare strumenti astronomici antichi e godere di una vista mozzafiato sui canali della città. Il percorso museale racconta l'evoluzione della scienza celeste e il legame profondo tra Padova e lo studio dell'universo."
+    "Situata in un'antica torre del castello dei Carraresi, questa struttura è stata trasformata nel XVIII secolo in un osservatorio astronomico all'avanguardia. Oggi ospita un affascinante museo dove è possibile ammirare strumenti astronomici antichi e godere di una vista mozzafiato sui canali della città. Il percorso museale racconta l'evoluzione della scienza celeste e il legame profondo tra Padova e lo studio dell'universo.",
+    "La trattoria più antica di Padova. Atmosfera accogliente e famigliare che ricrea l'atmosfera originale del XIX secolo. Sapori genuini della tradizione veneta, legata alle stagioni e con prodotti sempre freschi a km 0. Situata in una zona tranquilla e lontana dalle rotte turistiche più battute."
     ],
     "hours": _hours,
     "steps": _steps,
     "crowded": _crowded,
     "opened": _opened,
     "image": ["lib/images/mete/CappellaScrovegni.jpg",
+      "lib/images/mete/GiardiniArena.jpg",
       "lib/images/mete/BasilicaAntonio.jpg",
+      "lib/images/mete/Pazientina.jpg",
       "lib/images/mete/PratoValle.jpg",
       "lib/images/mete/PalazzoRagione.jpg",
       "lib/images/mete/OrtoBotanico.jpg",
@@ -59,7 +66,9 @@ class Places{
       "lib/images/mete/PiazzaFrutta.jpg",
       "lib/images/mete/CaffePedrocchi.jpg",
       "lib/images/mete/SanGiovanni.jpg",
-      "lib/images/mete/Specola.jpg"],
+      "lib/images/mete/Specola.jpg",
+      "lib/images/mete/trattoria.jpg",
+      ],
     "pile": batt
 
 };
