3592098b96b0d73c4612e1965404806608006f96
Mattia <mattia.bovo.1@studenti.unipd.it>
2026-07-10 19:54:49 +0200

    dati

--- lib/utils/impact.dart
+++ lib/utils/impact.dart
@@ -83,47 +83,7 @@ class Impact{
   } //_getAndStoreTokens
 
 
-Future<List<Steps>?> requestData() async {
-    
-    List<Steps>? result;
-    
-    //Get the stored access token
-    final sp = await SharedPreferences.getInstance();
-    var access = sp.getString('access');
 
-    //If access token is expired, refresh it
-    if(JwtDecoder.isExpired(access!)){
-      await refreshTokens();
-      access = sp.getString('access');
-    }//if
-
-    //Create the (representative) request
-    final ieri = DateTime.now().subtract(Duration(days: 1));
-    final day = DateFormat('yyyy-MM-dd').format(ieri);
-    
-    final url = '${Impact.baseUrl}${Impact.stepsEndpoint}${Impact.patientUsername}/day/$day/';
-    final headers = {HttpHeaders.authorizationHeader: 'Bearer $access'};
-
-    //Get the response
-    print('Calling: $url');
-    final response = await http.get(Uri.parse(url), headers: headers);
-    
-    //if OK parse the response, otherwise return null
-    if (response.statusCode == 200) {
-      final decodedResponse = jsonDecode(response.body);
-      result = [];
-      
-      for (var i = 0; i < decodedResponse['data']['data'].length; i++) {
-        result.add(Steps.fromJson(decodedResponse['data']['date'], decodedResponse['data']['data'][i]));
-      }//for
-    } //if
-    else{
-      result = null;
-    }//else
-    
-    return result;
-
-  } //_requestData
 
 
 }
